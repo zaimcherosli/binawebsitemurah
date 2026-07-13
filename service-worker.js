0415bc4a77cf8ae831d240affb5f727553ff6b22
zaimcherosli <huzaimrosli@gmail.com>
2026-07-13 12:28:24 +0800

    fix: resolve PWA app start_url redirection error on Cloudflare Pages

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-const CACHE_NAME = 'auracraft-cache-v1';
+const CACHE_NAME = 'auracraft-cache-v2';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html',
   './style.css',
   './script.js',
   './manifest.json',
